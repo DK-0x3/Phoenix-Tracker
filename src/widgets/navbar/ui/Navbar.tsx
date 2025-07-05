83e93a5d6ac8010a108723d710c721a0e9e6c09c
Dmitry Kezarev <dkezarev516@gmail.com>
2025-07-05 23:49:48 +0500

    Дорабатывает Navbar

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,11 +1,18 @@
 import styles from './Navbar.module.scss';
 import { LangSwitcher } from '../../../shared/ui/lang-switcher/LangSwitcher';
-import { ReactComponent as Logo } from '../../../shared/assets/svg/phoenix-logo.svg';
+import Logo from '../../../shared/assets/svg/phoenix-logo.svg';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
 	return (
 		<nav className={styles.Navbar}>
-			<Logo/>
+			<Logo className={styles.Logo}/>
+			<Link to="/">
+				<p>
+					{/* eslint-disable-next-line max-len,i18next/no-literal-string */}
+					<span className={styles.highlightTitle}>P</span>hoenix <span className={styles.highlightTitle}>T</span>racker
+				</p>
+			</Link>
 			<LangSwitcher/>
 		</nav>
 	);
